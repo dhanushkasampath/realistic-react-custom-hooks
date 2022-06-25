15c4303d299ae193b26f5e0dc2b791e4de92e6b4
dsampath <dsampath@wiley.com>
2022-06-25 20:01:32 +0530

    extracted the common code for send requests and created a custom hook

--- src/App.js
+++ src/App.js
@@ -1,43 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useHttp from "./hooks/use-http";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-prep-b4fd7-default-rtdb.firebaseio.com/tasks.json'
-      );
+  //here we rename the sendRequests to fetchTask inside the App component
+  const {isLoading, error, sendRequest: fetchTasks} = useHttp();
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
+  useEffect(() => {
 
-      const data = await response.json();
-
+    const transformTasks = (taskObj) => {
       const loadedTasks = [];
 
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
       }
 
       setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
+    };
 
-  useEffect(() => {
-    fetchTasks();
-  }, []);
+    fetchTasks(
+        {url: 'https://react-prep-b4fd7-default-rtdb.firebaseio.com/tasks.json'},
+        transformTasks);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
